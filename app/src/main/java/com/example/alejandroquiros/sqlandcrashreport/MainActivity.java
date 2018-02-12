a6c15504a5e74c200d6c766848d830c2a1741bd4
Alex-Kun <alejandro.quiros@live.u-tad.com>
2018-02-12 09:41:53 +0100

    insert del contacto, y lectura correctamente

--- app/src/main/java/com/example/alejandroquiros/sqlandcrashreport/MainActivity.java
+++ app/src/main/java/com/example/alejandroquiros/sqlandcrashreport/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.alejandroquiros.sqlandcrashreport;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.alejandroquiros.sqlandcrashreport.sqlliteadmin.Contact;
+import com.example.alejandroquiros.sqlandcrashreport.sqlliteadmin.DatabaseHandler;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHandler databaseHandler = new DatabaseHandler(this);
+    Contact contact = new Contact(4,"Alejandro", "123445634");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Añadimos un contacto
+        databaseHandler.addContact(contact);
+        Contact c1 = databaseHandler.getContact(4);
+        Log.v("Contacto SQL", c1.getName()+" --- "+c1.getPhoneNumber());
     }
+
+
 }
